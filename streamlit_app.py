29f7e5c6370d83b71ef8e126e9b9e9b09035001d
tbcs50play <155087499+tbcs50play@users.noreply.github.com>
2023-12-29 15:11:16 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,16 @@ import streamlit as st
 with st.chat_message("user"):
     st.write("Hello, World👋")
 
+if st.button('Click Me'):
+    st.write('Why hello there')
+else:
+    st.write('You know you want to click the button above')
+
+st.button("Reset", type="primary")
+
+
 """
 # This is my own app! - Travis
-Let's get crackin' - 2
+Let's get crackin'
 """
+
